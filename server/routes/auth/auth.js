81126e844dd87ae104fd0f1dfc0a8a0d3226a0d7
baxromxoja16 <aglamof@mail.ru>
2022-07-06 16:14:00 +0500

    feat: auth admin complate

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -1,15 +1,26 @@
 const { Router } = require("express");
 const router = Router();
-const { auth } = require("../../controllers/auth/auth.js");
+const { register, login } = require("../../controllers/auth/auth.js");
 const { check } = require("express-validator");
 
 router.post(
-  "/",
+  "/register",
   [
     check("email", "Noto'g'ri email").isEmail(),
     check("password", "Noto'g'ri parol").isLength({ min: 6 }),
   ],
-  auth
+  register
+  // =========== POST   http://localhost:3000/auth/register
+);
+
+router.post(
+  "/login",
+  [
+    check("email", "Noto'g'ri email").isEmail(),
+    check("password", "Parol noto'g'ri").exists(),
+  ],
+  login
+  // =========== POST  http://localhost:3000/auth/login
 );
 
 module.exports = router;
